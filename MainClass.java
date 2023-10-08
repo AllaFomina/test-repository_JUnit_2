6bc251e40529e0ac083916fe20789333d87d248b
AllaFomina <afomina102013@gmail.com>
2023-10-08 13:05:50 +0500

    MainClass_JUnit_2

---
+++ MainClass.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+public class MainClass
+{
+    private int class_number = 20;
+
+    @Test
+    public void getClassNumber()
+    {
+        int class_number = this.class_number;
+        System.out.println(class_number);
+
+
+    }
+
+
+}
